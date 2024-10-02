17c9a16e4d9b9ac5a50485132c0fc51dc4052c61
FLOPPATO <95508641+FLOPPATO@users.noreply.github.com>
2024-10-02 11:51:02 +0200

    Update main.java

--- Teatro/main.java
+++ Teatro/main.java
@@ -7,29 +7,29 @@ public class main {
 	public static void main(String[] args) {
 		teatro Teatro = new teatro(15,46);
 		
-        Thread[] spettatori = new Thread[7]; // numero spect.
-        for (int i = 0; i < spettatori.length; i++) {
-            spettatori[i] = new Thread(new spettatore(Teatro, "Spettatore " + (i + 1)));
-        }
-        
-        for (Thread spettatore : spettatori) { // foreach
-            spettatore.start();
-        }
-        
-        try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {}
-        System.out.println("Lo spettacolo sta per iniziare");
-        
-        for (Thread spettatore : spettatori) {
-            try {
-				spettatore.join();
+	        Thread[] spettatori = new Thread[7]; // numero spect.
+	        for (int i = 0; i < spettatori.length; i++) {
+	            spettatori[i] = new Thread(new spettatore(Teatro, "Spettatore " + (i + 1)));
+	        }
+	        
+	        for (Thread spettatore : spettatori) { // foreach
+	            spettatore.start();
+	        }
+	        
+	        try {
+				Thread.sleep(10000);
 			} catch (InterruptedException e) {}
-        }
-        
-        Teatro.mappaPosti();
-        
-        System.out.println("Posti disponibili: " + Teatro.postiavanzati());
+	        System.out.println("Lo spettacolo sta per iniziare");
+	        
+	        for (Thread spettatore : spettatori) {
+	            try {
+					spettatore.join();
+				} catch (InterruptedException e) {}
+	        }
+	        
+	        Teatro.mappaPosti();
+	        
+	        System.out.println("Posti disponibili: " + Teatro.postiavanzati());
 
 	}
 
